d6472fe12ac2a274e5c08642ca0695f0893e9079
usrJKim <jnhkm11581@kaist.ac.kr>
2025-08-05 21:02:39 +0900

    test

--- outputs/prober/test/plot_gather.py
+++ outputs/prober/test/plot_gather.py
@@ -1,15 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import glob
-import argparse
 
+def labelling(file):
+    if 'gemv' in file:
+        return 'GEMV', "#1f77b4"
+    elif 'sharedMem' in file:
+        return 'Shared memory',"#ff7f0e"
+    elif 'sfu' in file:
+        return 'SFU', "#2ca02c"
+    elif 'fp' in file:
+        return 'Floating point',"#d62728"
+
+csv_files = glob.glob("./data/*.csv")
 plt.figure(figsize=(12,6))
 
-df = pd.read_csv("old.csv")
-plt.plot(df['time_ms'], df['power_w'], label="old",color="blue")
-
-df = pd.read_csv("new.csv")
-plt.plot(df['time_ms'], df['power_w'], label="new",color="orange")
+for file in csv_files:
+    df = pd.read_csv(file)
+    label,color = labelling(file)
+    plt.plot(df['time_ms'], df['power_w'], label=label,color=color)
 
 plt.xlabel("Time (msec)")
 plt.ylabel('Power (W)')
@@ -17,4 +26,4 @@ plt.title('Power Consumption over Time')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("./summary.png")
+plt.savefig("./summary_plot.png")
