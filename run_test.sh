8119abe680c94f00b295785224ebba5c5869f262
usrJKim <jnhkm11581@kaist.ac.kr>
2025-08-21 17:20:14 +0900

    edit: new test condition

--- run_test.sh
+++ run_test.sh
@@ -4,26 +4,22 @@
 #====Configuration======
 PROBER_MIG_UUID="MIG_GPU-xxxxx"
 MODEL_MIG_UUID="MIG_GPU-xxxxx"
-MIG=1
-BIT=1
-#======================= 
+#=======================
 
-for ITER in 1 2 3 4 5 6 7 8 9 10; do
-  PROBER_OUT="MIG${MIG}g_test_${ITER}.csv"
+for MIG in 1 2 3 4; do
+  PROBER_OUT="MIG${MIG}g_test_IntegerSum.csv"
   # Run prober
   sudo docker run --rm --name prober-container \
-  --gpus "device=${PROBER_MIG_UUID}" \
-  -v $(pwd)/outputs:/outputs \
-  prober-image /outputs/prober/${PROBER_OUT} &
-
-  PROBER_PID=$!
+    --gpus "device=${PROBER_MIG_UUID}" \
+    -v $(pwd)/outputs:/outputs \
+    prober-image /outputs/prober/${PROBER_OUT} &
 
   sleep 0.1 # wait to run prober first
 
   #RUN test
   sudo docker run -it --rm \
-  --gpus "device=${MODEL_MIG_UUID}" \
-  test-image "input_files/mig${MIG}g/labels${BIT}bit.csv"
-  
+    --gpus "device=${MODEL_MIG_UUID}" \
+    test-image 100 100 "input_files/pulse_mig${MIG}g.csv"
+
   sudo docker stop prober-container
 done
